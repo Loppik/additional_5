694e97551ddacba7236aae00291da6d34ba2bc49
Alex <shantyr_qq@mail.ru>
2018-02-24 15:54:20 +0300

    production

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arBracketsRegExp = [];
+  bracketsConfig.forEach(function(elem) {
+    arBracketsRegExp.push(new RegExp("\\" + elem[0] + "\\" + elem[1], "g"));
+  });
+  let strBefore, count = 0, size = arBracketsRegExp.length, res = false;
+  while(true) {
+    arBracketsRegExp.forEach(function(elem) {
+      do {
+        strBefore = str;
+        str = str.replace(elem, '');
+        count += 1;
+      } while(strBefore != str);
+    });
+    if (count == size) {
+      if (str.length == 0)
+        res = true;
+      break;
+    }
+    count = 0;
+  }
+  return res;
 }
